393618d6e11faf51d8fe3c50496280bbcf2ade53
toureydaou <ttoureydaou@gmail.com>
2024-01-12 08:58:42 +0000

    using drugsRoutes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const { API_PORT } = require("./config");
 
 const express = require("express");
+const { drugsRoutes } = require("./routes/drugsRoutes");
 const app = express();
-
-
 
 app.listen(API_PORT, "localhost", () => {
   console.log(`Server running on port ${API_PORT}`);
 });
+
+app.use(drugsRoutes);
