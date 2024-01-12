3239390086e2c1d501e951af8f7dcf92023091f2
toureydaou <ttoureydaou@gmail.com>
2024-01-12 02:12:38 +0000

    api controller

---
+++ controllers/drugsController.js
@@ -0,0 +1,10 @@
+const db = require("../database");
+const Drugs = require("../models/drugs");
+
+const getAllDrugs = async () => {
+  console.log("hello");
+};
+
+module.exports = {
+  getAllDrugs: getAllDrugs,
+};
