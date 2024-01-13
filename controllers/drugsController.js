6d4599ecb75cfff5eacabdfd61463fc45f99bc52
toureydaou <ttoureydaou@gmail.com>
2024-01-13 18:06:51 +0000

    sercha by therapy and sort products results and therapies by name

--- controllers/drugsController.js
+++ controllers/drugsController.js
@@ -1,10 +1,89 @@
-const db = require("../database");
+const { Op } = require("sequelize");
 const Drugs = require("../models/drugs");
 
-const getAllDrugs = async () => {
-  console.log("hello");
+const getAllDrugs = async (req, res) => {
+  try {
+    await Drugs.findAll({
+      where: {
+        name: {
+          [Op.substring]: req.query.name ? req.query.name.toUpperCase() : "",
+        },
+      },
+      order: [["name", "ASC"]],
+    }).then((data) => {
+      if (data != null) {
+        res.status(200).json({ drugs: data });
+      } else {
+        res.status(404).json({ error: "No drug found !" });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+const getDrugById = async (req, res) => {
+  try {
+    await Drugs.findByPk(req.params.id).then((data) => {
+      if (data != null) {
+        res.status(200).json({ drug: data });
+      } else {
+        res.status(404).json({ error: "No drug found !" });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+const getDrugsAllTherapy = async (req, res) => {
+  try {
+    await Drugs.findAll({
+      attributes: ["therapy"],
+      order: [["therapy", "ASC"]],
+    }).then((data) => {
+      if (data != null) {
+        const therapies = JSON.stringify(data);
+
+        const listTherapies = JSON.parse(therapies);
+
+        const setTherapies = new Set(listTherapies.map(JSON.stringify));
+
+        // Convertir l'ensemble en tableau
+        const newlistTherapies = Array.from(setTherapies).map(JSON.parse);
+
+        res.status(200).json({ therapies: newlistTherapies });
+      } else {
+        res.status(404).json({ error: "Ressource not found" });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "An error occurs" });
+  }
+};
+
+const getDrugsByTherapy = async (req, res) => {
+  try {
+    await Drugs.findAll({
+      where: {
+        therapy: {
+          [Op.eq]: req.query.therapy,
+        },
+      },
+      order: [["name", "ASC"]],
+    }).then((data) => {
+      res.status(200).json({ drugs: data });
+    });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurs" });
+  }
 };
 
 module.exports = {
   getAllDrugs: getAllDrugs,
+  getDrugById: getDrugById,
+  getDrugsAllTherapy: getDrugsAllTherapy,
+  getDrugsByTherapy: getDrugsByTherapy,
 };
--- routes/drugsRoutes.js
+++ routes/drugsRoutes.js
@@ -1,10 +1,18 @@
-const { getAllDrugs } = require("../controllers/drugsController");
+const {
+  getAllDrugs,
+  getDrugById,
+  getDrugsAllTherapy,
+  getDrugsByTherapy,
+} = require("../controllers/drugsController");
 
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", getAllDrugs);
+router.get("/therapies", getDrugsAllTherapy);
+router.get("/therapy", getDrugsByTherapy);
+router.get("/:id", getDrugById);
 
 module.exports = {
   drugsRoutes: router,
